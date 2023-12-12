3e6a9dc0484af4c4cc646a5f0a78991f3022069e
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-12-12 10:45:47 +0300

    Update Promises.js

--- src/Promises.js
+++ src/Promises.js
@@ -1,43 +1,55 @@
-let users = [
-  {
-    name: "Bella Rose",
-    age: 23,
-    email: "bellarose@gmail.com",
-  },
-  {
-    name: "Lucca Ress",
-    age: 33,
-    email: "luccaress@gmail.com",
-  },
-];
+import React from "react";
 
-function getUsers() {
-  setTimeout(() => {
-    let output = "";
-    users.map((user, key) => {
-      output += `<p key=${index}>Name: ${user}</p>`;
-    });
-  }, 1000);
-  let body = document.querySelector("#main");
-  body.innerHTML = output;
-}
+export default function Promises() {
+  let users = [
+    {
+      name: "Bella Rose",
+      age: 23,
+      email: "bellarose@gmail.com",
+    },
+    {
+      name: "Lucca Ress",
+      age: 33,
+      email: "luccaress@gmail.com",
+    },
+  ];
 
-function createUser(user) {
-  return new Promise((resolve, reject) => {
+  function getUsers() {
     setTimeout(() => {
-      users.push(user);
+      let output = "";
+      users.map((user, key) => {
+        output += `<p key=${index}>Name: ${user}</p>`;
+      });
+    }, 1000);
+    let body = document.querySelector("#main");
+    body.innerHTML = output;
+  }
 
-      const error = false;
+  function createUser(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        users.push(user);
 
-      if (!error) {
-        resolve();
-      } else {
-        reject("Error: Something went wrong!");
-      }
-    }, 2000);
-  });
+        const error = false;
+
+        if (!error) {
+          resolve();
+        } else {
+          reject("Error: Something went wrong!");
+        }
+      }, 2000);
+    });
+  }
+
+  createUser({
+    name: "Lucia Masu",
+    age: 31,
+    emial: "luciamasu@gmail.com",
+  }).then(getUsers);
+
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
 }
-
-createUser({ name: "Lucia Masu", age: 31, emial: "luciamasu@gmail.com" }).then(
-  getUsers
-);
