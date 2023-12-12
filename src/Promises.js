262c36eff450bc9241bdf78f75005f7d4897a074
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-12-12 10:42:10 +0300

    Create Promises.js

---
+++ src/Promises.js
@@ -0,0 +1,43 @@
+let users = [
+  {
+    name: "Bella Rose",
+    age: 23,
+    email: "bellarose@gmail.com",
+  },
+  {
+    name: "Lucca Ress",
+    age: 33,
+    email: "luccaress@gmail.com",
+  },
+];
+
+function getUsers() {
+  setTimeout(() => {
+    let output = "";
+    users.map((user, key) => {
+      output += `<p key=${index}>Name: ${user}</p>`;
+    });
+  }, 1000);
+  let body = document.querySelector("#main");
+  body.innerHTML = output;
+}
+
+function createUser(user) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      users.push(user);
+
+      const error = false;
+
+      if (!error) {
+        resolve();
+      } else {
+        reject("Error: Something went wrong!");
+      }
+    }, 2000);
+  });
+}
+
+createUser({ name: "Lucia Masu", age: 31, emial: "luciamasu@gmail.com" }).then(
+  getUsers
+);
