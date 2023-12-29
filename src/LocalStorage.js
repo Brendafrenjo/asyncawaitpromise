1a6527e880c92f6b5c52e3337f6ce26ad4dbe87c
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-12-29 18:48:36 +0300

    Updated LocalStorage.js

--- src/LocalStorage.js
+++ src/LocalStorage.js
@@ -6,6 +6,8 @@ export default function LocalStorage() {
     firstName: "",
     lastName: "",
     email: "",
+    age: "",
+    roll: "",
   });
 
   // Load data from localStorage on component mount
@@ -29,7 +31,9 @@ export default function LocalStorage() {
     if (
       formData.firstName.trim() === "" ||
       formData.lastName.trim() === "" ||
-      formData.email.trim() === ""
+      formData.email.trim() === "" ||
+      formData.age.trim() === "" || 
+      formData.roll.trim() === ""
     ) {
       alert("All fields are required");
       return;
@@ -46,6 +50,8 @@ export default function LocalStorage() {
       firstName: "",
       lastName: "",
       email: "",
+      age: "",
+      roll: "",
     });
   }
 
@@ -59,7 +65,7 @@ export default function LocalStorage() {
           id="firstName"
           name="firstName"
           placeholder="Enter your first name"
-          className="form-control info-form"
+          className="form-control info-form text-center"
           onChange={handleChange}
           value={formData.firstName}
         />
@@ -69,7 +75,7 @@ export default function LocalStorage() {
           id="lastName"
           name="lastName"
           placeholder="Enter your surname"
-          className="form-control info-form"
+          className="form-control info-form text-center"
           onChange={handleChange}
           value={formData.lastName}
         />
@@ -79,18 +85,21 @@ export default function LocalStorage() {
           id="email"
           name="email"
           placeholder="name@example.com"
-          className="form-control info-form"
+          className="form-control info-form text-center"
           onChange={handleChange}
           value={formData.email}
         />
-        <small id="emailHelp" className="form-text text-muted">
+        <small id="emailHelp" className="form-text text-muted text-center">
           We'll never share your email with anyone else.
         </small>
-        <br/>
+        <br />
         <label htmlFor="email">Age:</label>
         <select
-          className="custom-select my-1 mr-sm-2 form-control info-form"
-          id="inlineFormCustomSelectPref"
+          className="custom-select my-1 mr-sm-2 form-control info-form text-center"
+          id="age"
+          name="age"
+          onChange={handleChange}
+          value={formData.age}
         >
           <option selected>Student age</option>
           {Array.from({ length: 36 }, (_, index) => (
@@ -102,10 +111,10 @@ export default function LocalStorage() {
         <label htmlFor="email">Roll Number:</label>
         <input
           type="text"
-          id="email"
-          name="email"
+          id="roll"
+          name="roll"
           placeholder="Student's roll"
-          className="form-control info-form"
+          className="form-control info-form text-center"
           onChange={handleChange}
           value={formData.roll}
         />
