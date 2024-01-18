75f3f4d14c23043ca0e6f8e1c1e6f44b2fac86b9
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-18 23:08:34 +0300

    Update LogInForm.js

--- src/LogInForm.js
+++ src/LogInForm.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 export default function LogInForm() {
-    return (
-        <div>
-            <h1>Log In Form</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Log In Form</h1>
+      <form>
+        <input
+          type="firstName"
+          id="firstName"
+          className="firstName form-control"
+        />
+        <input
+          type="password"
+          id="passwordInput"
+          className="passwordInput form-control"
+        />
+        <input type="checkbox" id="checkBox" />
+        <label htmlFor="checkBox">Show Password</label>
+      </form>
+    </div>
+  );
 }
