e806e795d00c78ae9f01bfdba54156e72f638e88
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-19 23:55:13 +0300

    Updated LogInForm

--- src/LogInForm.js
+++ src/LogInForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./LogInForm.css";
 
 export default function LogInForm() {
   const [formData, setFormData] = useState({ firstName: "", password: "" });
@@ -21,26 +22,26 @@ export default function LogInForm() {
   }
 
   return (
-    <div>
+    <div className="LogInForm">
       <h1>Log In Form</h1>
       {formInfo && "Information Sucessfully submitted"}
       <form onSubmit={handleSubmit}>
         <input
           type="firstName"
           id="firstName"
-          className="firstName form-control"
+          className="firstName form-control mb-2"
           onClick={handleChange}
           value={formData.firstName}
         />
         <input
           type="password"
           id="passwordInput"
-          className="passwordInput form-control"
+          className="passwordInput form-control mb-2"
           onClick={handleChange}
           value={formData.password}
         />
         <input type="checkbox" id="checkBox" />
-        <label htmlFor="checkBox">Show Password</label>
+        <label htmlFor="checkBox" className="ms-1">Show Password</label>
       </form>
     </div>
   );
