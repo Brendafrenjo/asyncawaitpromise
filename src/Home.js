a4b28f5d1db67d5d88d898cc43f4759d17cba4e4
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-19 23:35:57 +0300

    Updated LogInForm

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Promises from "./Promises";
-import LocalStorage from "./LocalStorage"
+import LocalStorage from "./LocalStorage";
+import LogInForm from "./LogInForm";
 
 export default function Home() {
   return (
     <div>
+      <LogInForm />
       <LocalStorage />
       <Promises />
     </div>
--- src/LogInForm.js
+++ src/LogInForm.js
@@ -1,19 +1,39 @@
 import React from "react";
 
 export default function LogInForm() {
+  const [formData, setFormData] = useState({ firstName: "", password: "" });
+  const [formInfo, setFormInfo] = useState(false);
+
+  function handleChange(e) {
+    e.preventDefault();
+  }
+
+  function handleSubmit() {
+    if (firstName && password) {
+      setFormInfo(true);
+    } else {
+      alert("Provide all relevant information");
+    }
+  }
+
   return (
+    {formInfo && "Information Sucessfully submitted"}
     <div>
       <h1>Log In Form</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="firstName"
           id="firstName"
           className="firstName form-control"
+          onClick={handleChange}
+          value={formData.firstName}
         />
         <input
           type="password"
           id="passwordInput"
           className="passwordInput form-control"
+          onClick={handleChange}
+          value={formData.password}
         />
         <input type="checkbox" id="checkBox" />
         <label htmlFor="checkBox">Show Password</label>
